2c9ae01a88fc28d497b7db5717215d7743326a8b
James Goldie <james.goldie@monash.edu>
2021-11-08 15:50:16 +1100

    Switch to d3-fetch for CSV parsing

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import '../node_modules/leaflet/dist/leaflet.css'
 import 'leaflet'
 import '../node_modules/leaflet-sidebar-v2/css/leaflet-sidebar.css'
 import 'leaflet-sidebar-v2'
-import * as csvParser from 'papaparse'
-import csvFile from './data/irena.csv'
+import {csv} from 'd3-fetch'
+import csvFile from './data/irena-totals.csv'
 
 console.log("Parcel linking correctly! Aha!")
 
@@ -38,11 +38,23 @@ sidebar.on("content", function (ev) {
 /* load the data and initialise its layers */
 console.log(csvFile);
 // const csvFile = new URL('data/irena.csv', import.meta.url);
-csvParser.parse(csvFile, {
-  download: true,
-  complete: function(data) {
-    console.log(data);
+// csvParse(csvFile, {
+//   download: true,
+//   header: true,
+//   complete: function(data) {
+//     console.log(data);
 
-    // marker adding goes here!
-  }
+
+//   }
+// })
+
+csv(csvFile).then((d) => {
+  console.log(d);
+
+  //     // 1. initialise + populate menus
+
+  //     // 2. add menu callbacks:
+  //     //   - clear markers
+  //     //   - recreate markers based on selections
+
 })
