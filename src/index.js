428d5b451f6c8c10ac6e29b085c7ed60211116a2
James Goldie <james.goldie@monash.edu>
2021-11-10 16:34:47 +1100

    Basic MapboxGL map

--- src/index.js
+++ src/index.js
@@ -1,60 +1,15 @@
 
-import '../node_modules/leaflet/dist/leaflet.css'
-import 'leaflet'
-import '../node_modules/leaflet-sidebar-v2/css/leaflet-sidebar.css'
-import 'leaflet-sidebar-v2'
-import {csv} from 'd3-fetch'
-import csvFile from './data/irena-totals.csv'
+// import {csv} from 'd3-fetch'
+// import csvFile from './data/irena-totals.csv'
+
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 console.log("Parcel linking correctly! Aha!")
 
-let map = L.map("map", {
-  zoom: 5,
-  center: { lat: -37, lon: 140 } 
-})
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiamFtZXMtZ29sZGllIiwiYSI6ImNrMjl1cjJzeDEwOXkzY28ybzZueHZvMG8ifQ.7bZwZ_XjBfm8kRDlkTlgZA';
 
-L.tileLayer("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  }).addTo(map)
-
-
-/* initalise the sidebar */
-
-// create the sidebar instance and add it to the map
-var sidebar = L.control.sidebar({ container: "sidebar" })
-  .addTo(map)
-  .open("home");
-
-// be notified when a panel is opened
-sidebar.on("content", function (ev) {
-    switch (ev.id) {
-        case "autopan":
-        sidebar.options.autopan = true;
-        break;
-        default:
-        sidebar.options.autopan = false;
-    }
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
 });
-
-/* load the data and initialise its layers */
-console.log(csvFile);
-// const csvFile = new URL('data/irena.csv', import.meta.url);
-// csvParse(csvFile, {
-//   download: true,
-//   header: true,
-//   complete: function(data) {
-//     console.log(data);
-
-
-//   }
-// })
-
-csv(csvFile).then((d) => {
-  console.log(d);
-
-  //     // 1. initialise + populate menus
-
-  //     // 2. add menu callbacks:
-  //     //   - clear markers
-  //     //   - recreate markers based on selections
-
-})
