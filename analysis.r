58e7cc51dfaf36fd3a6208717b48be8b16191aab
James Goldie <james.goldie@monash.edu>
2021-11-15 15:37:23 +1100

    geoJSON analysis

--- analysis.r
+++ analysis.r
@@ -34,27 +34,57 @@ irena <-
   mutate(
     generated_gwh = parse_number(generated_gwh),
     capacity_mw = parse_number(capacity_mw)) %>%
-  # add the centroids on
-  left_join(country_list, by = c("iso" = "iso3"))
+  # add the centroids on and create geometry
+  left_join(country_list, by = c("iso" = "iso3")) %>%
+  write_csv(here("src", "data", "irena.csv"))
+  
+irena %>%
+  # now output as geojson points
+  filter(!is.na(centroid.lon), !is.na(centroid.lat)) %>%
+  st_as_sf(coords = c("centroid.lon", "centroid.lat"), crs = 4326) %>%
+  st_write(here("src", "data", "irena.geojson"))
 
-write_csv(irena, here("src", "data", "irena.csv"))
-write_json(irena, here("src", "data", "irena.json"))
 
 # renewables vs non-renewables
 totals <-
   irena %>%
-  mutate(renewable = recode(renewable_status, "Total Renewable" = TRUE, "Total Non-Renewable" = FALSE)) %>%
+  mutate(renewable = recode(renewable_status, "Total Renewable" = "renewable", "Total Non-Renewable" = "nonrenewable")) %>%
   select(-renewable_status) %>%
   group_by(iso, country, region, year, renewable) %>%
   summarise(
     centroid.lat = centroid.lat[1],
     centroid.lon = centroid.lon[1],
-    generated_gwh = sum(generated_gwh, na.rm = TRUE),
-    capacity_mw = sum(capacity_mw, na.rm = TRUE)) %>%
-  ungroup()
+    totalgen_gwh = sum(generated_gwh, na.rm = TRUE),
+    totalcap_mw = sum(capacity_mw, na.rm = TRUE)) %>%
+  ungroup() %>%
+  write_csv(here("src", "data", "irena-totals.csv"))
 
-write_csv(totals, here("src", "data", "irena-totals.csv"))
-write_json(totals, here("src", "data", "irena-totals.json"))
+# now output as geojson points
+# (sf doesn't support nested geojson output, so we'll have to do it super wide)
+totals %>%
+  # create nested structure...
+  pivot_wider(
+    names_from = c(renewable, year),
+    values_from = c(totalgen_gwh, totalcap_mw),
+    names_sep = ".") %>%
+  # nest(
+  #   totalgen_gwh_nonrenewable =
+  #     c(year, starts_with("totalgen_gwh_nonrenewable")),
+  #   totalgen_gwh_renewable =
+  #     c(year, starts_with("totalgen_gwh_renewable")),
+  #   totalcap_mw_nonrenewable =
+  #     c(year, starts_with("totalcap_mw_nonrenewable")),
+  #   totalcap_mw_renewable =
+  #     c(year, starts_with("totalcap_mw_renewable"))) %>%
+  # ... then clear out blank countries and export
+  filter(!is.na(centroid.lon), !is.na(centroid.lat)) %>%
+  st_as_sf(coords = c("centroid.lon", "centroid.lat"), crs = 4326) %>%
+  select(iso, country, region, geometry, everything()) -> test %>%
+  st_write(here("src", "data", "irena-totals.geojson"))
+
+# summarise the figures (for setting scales in mapbox)
+totals %>% filter(renewable == "renewable") %>% summary()
+totals %>% filter(renewable == "nonrenewable") %>% summary()
 
 # test case: total renewable figures in 2019
 renewables_2019 <-
